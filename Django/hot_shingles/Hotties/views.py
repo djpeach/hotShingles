709bd5390ef8542e4c3cdc08b22df3470089b3c1
Daniel Peach <dpeaches96@gmail.com>
2019-01-11 21:46:19 -0500

    Created hotties routes

--- Django/hot_shingles/Hotties/views.py
+++ Django/hot_shingles/Hotties/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from . import models
 
-# Create your views here.
+
+class HottieListAll(generic.ListView):
+    model = models.Hottie
+
+
+class HottieDetail(generic.DetailView):
+    model = models.Hottie
