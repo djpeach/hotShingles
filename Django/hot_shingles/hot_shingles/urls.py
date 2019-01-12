77410c5cfdf73ae2699a6896b17e315329be737a
Daniel Peach <dpeaches96@gmail.com>
2019-01-11 21:50:19 -0500

    fixed hotties route

--- Django/hot_shingles/hot_shingles/urls.py
+++ Django/hot_shingles/hot_shingles/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shingles/', include('Shingles.urls')),
-    path('shingles/', include('Hotties.urls')),
+    path('hotties/', include('Hotties.urls')),
 ]
